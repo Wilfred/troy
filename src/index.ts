dcb4114259153dad114b76204be31963ed878ff6
Wilfred Hughes <me@wilfred.me.uk>
2026-02-07 22:49:05 +0000

    basic arg parsing

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("Hello, world!");
+import { parseArgs } from "node:util";
+
+const { values } = parseArgs({
+  options: {
+    prompt: { type: "string", short: "p" },
+  },
+});
+
+if (!values.prompt) {
+  console.error("Usage: troy --prompt <string>");
+  process.exit(1);
+}
+
+console.log(values.prompt);
