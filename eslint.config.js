b10fa7d56c7e89720103221b41009f3c7948d652
Wilfred Hughes <me@wilfred.me.uk>
2026-02-10 00:10:33 +0000

    Ban nested functions

--- eslint.config.js
+++ eslint.config.js
@@ -5,4 +5,17 @@ export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
   { ignores: ["dist/"] },
+  {
+    rules: {
+      "no-restricted-syntax": [
+        "error",
+        {
+          selector:
+            ":function FunctionDeclaration",
+          message:
+            "Nested named functions are not allowed. Move the function to the top level.",
+        },
+      ],
+    },
+  },
 );
